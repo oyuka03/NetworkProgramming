9d49c06e42afb40eb6c1319f1fb6934f3c4d8547
oyuka03 <k18301oo@apps.kct.ac.jp>
2019-04-26 10:25:11 +0900

    20190426

--- client.c
+++ client.c
@@ -1,6 +1,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <stdio.h>       /*** socket kansuugateigisareteiru***/
+#include <unistd.h>
+#include <stdio.h>      
+#include <string.h>     /*** socket kansuugateigisareteiru***/
+
+void commun(int sock){
+	char buf[256];
+	int len_r;
+	char*message="Hello serve03";
+	send(sock,message,strlen(message),0);
+	len_r=recv(sock,buf,256,0);
+	buf[len_r]='\0';
+	
+	
+	printf("%s\n",buf);
+}
+
+
 int main(int arg,char*argv){
 
 	int sock=socket(PF_INET,SOCK_STREAM,0);
@@ -8,10 +24,12 @@ int main(int arg,char*argv){
 	struct sockaddr_in target;
 	
 	target.sin_family=AF_INET;
-	target.sin_addr.s_addr=inet_addr("10.13.64.20"); /***hoid heseg ni@•ÏŠ·***/
+	target.sin_addr.s_addr=inet_addr("10.13.64.20"); /***hoid heseg niã€€å¤‰æ›***/
 	target.sin_port=htons(10001);
 	
 	connect(sock,(struct sockaddr*)&target,sizeof(target));
+	
+	commun(sock);
 	
 	close(sock);	
     return 0;
