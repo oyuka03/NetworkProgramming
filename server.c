dbcf890166106eab1842b2525651e4ee94b752de
oyuka03 <k18301oo@apps.kct.ac.jp>
2019-06-21 10:24:45 +0900

    20190621

--- server.c
+++ server.c
@@ -1,39 +1,52 @@
 #include<sys/socket.h>
 #include<unistd.h>
 #include<arpa/inet.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include<stdio.h>
-#define BUF_SIZE 256
 #include<string.h>
 
-void DieWithError(char*errorMessage){
+#define BUF_SIZE 256
+
+
+void DieWithError(char *errorMessage){
 	perror(errorMessage);
 	exit(1);
 }
 void commun(int sock){
 	char buf[BUF_SIZE];
 	int len_r;
-	if ((len_r=recv(sock,buf,BUF_SIZE, 0)) <= 0)                                    /***huleen avagch***/
-	         DieWithError("recv()failed");
+	char  *message ="‚¨•Ô‚µ‚Å-‚·";
+	if ((len_r=recv(sock, buf, BUF_SIZE,  0)) <= 0)                                    /***huleen avagch***/
+	         DieWithError("recv() failed");
 	buf[len_r]='\0';
 	printf("%s\n",buf);
-	if (send(sock,buf,strlen(buf),0)!= strlen(buf))
+	if (send(sock, message, strlen(message), 0)!= strlen(message))
 	         DieWithError("send()sent a message of unexpected bytes");
 }
-int main (int argc, char**argv){
-	int servSock = socket(PF_INET,SOCK_STREAM,0);
-	struct sockaddr_in servAddress;
+int main (int arg0, char **argv){
+	char buf[BUF_SIZE];
+	int i;
+	
 	struct sockaddr_in clientAddress;
 	unsigned int szClientAddr;
-	servAddress.sin_family=AF_INET;
-	servAddress.sin_addr.s_addr=htonl(INADDR_ANY);
-	servAddress.sin_port=htons(10001);
-	bind(servSock,(struct sockaddr*)&servAddress,sizeof(servAddress));
+	int cliSock;
+	int servSock = socket(PF_INET, SOCK_STREAM, 0);
+	
+	struct sockaddr_in servAddress;
+	
+	servAddress.sin_family        =PF_INET;
+	servAddress.sin_port          =(in_port_t)htons(80);
+	servAddress.sin_addr.s_addr   =htonl(INADDR_ANY);
+	
+	bind(servSock, (struct sockaddr*)&servAddress,sizeof(servAddress));
+	
 	listen(servSock,5);
+	
 	while(1){
 		szClientAddr=sizeof(clientAddress);
-	    int cliSock=accept(servSock,(struct sockaddr*)&clientAddress,&szClientAddr);
+	    cliSock=accept(servSock,(struct sockaddr*)&clientAddress, &szClientAddr);
 	    commun(cliSock);
+		close(cliSock);
     }
 	close(servSock);
 	return 0;
